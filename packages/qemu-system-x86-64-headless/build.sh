9898130ed0c3e398195cc593f8c5ff885c13a01b
Varun Palekar <varun.palekar@mendix.com>
2024-07-22 22:18:10 +0530

    Build qemu 2.9.1 [In progress]

--- packages/qemu-system-x86-64-headless/build.sh
+++ packages/qemu-system-x86-64-headless/build.sh
@@ -2,19 +2,22 @@ TERMUX_PKG_HOMEPAGE=https://www.qemu.org
 TERMUX_PKG_DESCRIPTION="A generic and open source machine emulator and virtualizer (headless)"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1:8.2.5
+TERMUX_PKG_VERSION=1:2.9.1
 TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://download.qemu.org/qemu-${TERMUX_PKG_VERSION:2}.tar.xz
-TERMUX_PKG_SHA256=65693b4a64f28be7501ed0dd3e042478780c516de3f17813d1c32ebb2eb249a9
-TERMUX_PKG_DEPENDS="dtc, glib, libbz2, libcurl, libgmp, libgnutls, libiconv, libjpeg-turbo, liblzo, libnettle, libnfs, libpixman, libpng, libslirp, libspice-server, libssh, libusb, libusbredir, ncurses, pulseaudio, qemu-common, resolv-conf, zlib, zstd"
+TERMUX_PKG_SHA256=4350b3d1d75f1e6a562d975687e181f61ceaf3ac94e6b629f87e24cbe680d03d
+TERMUX_PKG_DEPENDS="dtc, glib, libbz2, libcurl, libgmp, libgnutls, libiconv, libjpeg-turbo, liblzo, libnettle, libnfs, libpixman, libpng, libspice-server, libssh, libusb, libusbredir, ncurses, pulseaudio, qemu-common, resolv-conf, zlib, zstd, libcap"
 
 # Required by configuration script, but I can't find any binary that uses it.
-TERMUX_PKG_BUILD_DEPENDS="libtasn1"
+TERMUX_PKG_BUILD_DEPENDS="libtasn1, zlib, liblzo"
 
 TERMUX_PKG_CONFLICTS="qemu-system-x86_64-headless"
 TERMUX_PKG_REPLACES="qemu-system-x86_64-headless"
 TERMUX_PKG_PROVIDES="qemu-system-x86_64-headless"
 TERMUX_PKG_BUILD_IN_SRC=true
+# TERMUX_PKG_EXTRA_MAKE_ARGS="CFLAGS=\"-Wimplicit-function-declaration -Wimplicit-const-int-float-conversion -Wdeprecated-declarations -std=gnu99\" CXXFLAGS=\"-std=gnu++0x\""
+
+set -x
 
 termux_step_pre_configure() {
 	# Workaround for https://github.com/termux/termux-packages/issues/12261.
@@ -50,29 +53,11 @@ termux_step_configure() {
 
 	# System emulation.
 	QEMU_TARGETS+="aarch64-softmmu,"
-	QEMU_TARGETS+="arm-softmmu,"
-	QEMU_TARGETS+="i386-softmmu,"
-	QEMU_TARGETS+="m68k-softmmu,"
-	QEMU_TARGETS+="ppc64-softmmu,"
-	QEMU_TARGETS+="ppc-softmmu,"
-	QEMU_TARGETS+="riscv32-softmmu,"
-	QEMU_TARGETS+="riscv64-softmmu,"
 	QEMU_TARGETS+="x86_64-softmmu,"
 
-	# User mode emulation.
-	QEMU_TARGETS+="aarch64-linux-user,"
-	QEMU_TARGETS+="arm-linux-user,"
-	QEMU_TARGETS+="i386-linux-user,"
-	QEMU_TARGETS+="m68k-linux-user,"
-	QEMU_TARGETS+="ppc64-linux-user,"
-	QEMU_TARGETS+="ppc-linux-user,"
-	QEMU_TARGETS+="riscv32-linux-user,"
-	QEMU_TARGETS+="riscv64-linux-user,"
-	QEMU_TARGETS+="x86_64-linux-user"
-
-	CFLAGS+=" $CPPFLAGS"
-	CXXFLAGS+=" $CPPFLAGS"
-	LDFLAGS+=" -landroid-shmem -llog"
+	CFLAGS+=" $CPPFLAGS -I/data/data/com.termux/files/usr/include -std=gnu99"
+	CXXFLAGS+=" $CPPFLAGS -I/data/data/com.termux/files/usr/include -std=gnu++0x"
+	LDFLAGS+=" -landroid-shmem -llog -I/data/data/com.termux/files/usr/include"
 
 	# Note: using --disable-stack-protector since stack protector
 	# flags already passed by build scripts but we do not want to
@@ -93,47 +78,32 @@ termux_step_configure() {
 		--enable-gnutls \
 		--enable-nettle \
 		--disable-sdl \
-		--disable-sdl-image \
 		--disable-gtk \
 		--disable-vte \
 		--enable-curses \
-		--enable-iconv \
 		--enable-vnc \
 		--disable-vnc-sasl \
 		--enable-vnc-jpeg \
-		--enable-png \
 		--disable-xen \
 		--disable-xen-pci-passthrough \
 		--enable-virtfs \
 		--enable-curl \
-		--enable-fdt=system \
 		--enable-kvm \
-		--disable-hvf \
-		--disable-whpx \
 		--enable-libnfs \
 		--enable-lzo \
 		--disable-snappy \
 		--enable-bzip2 \
-		--disable-lzfse \
 		--disable-seccomp \
-		--enable-libssh \
-		--enable-bochs \
-		--enable-cloop \
-		--enable-dmg \
-		--enable-parallels \
-		--enable-qed \
-		--enable-slirp \
 		--enable-spice \
 		--enable-libusb \
 		--enable-usb-redir \
-		--disable-vhost-user \
-		--disable-vhost-user-blk-server \
-		--target-list="$QEMU_TARGETS"
+		--target-list="$QEMU_TARGETS" \
+		--python=/usr/bin/python2
 }
 
 termux_step_post_make_install() {
 	local i
-	for i in aarch64 arm i386 m68k ppc ppc64 riscv32 riscv64 x86_64; do
+	for i in aarch64 x86_64; do
 		ln -sfr \
 			"${TERMUX_PREFIX}"/share/man/man1/qemu.1 \
 			"${TERMUX_PREFIX}"/share/man/man1/qemu-system-${i}.1
